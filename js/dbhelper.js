8fa3376382138f060eac7ccdb8ad5f5d99be885e
Jessica Collins <jessicodes@gmail.com>
2018-11-16 09:56:22 -0800

    Updated DBHelper to use Heroku

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -7,8 +7,9 @@ class DBHelper {
      * Database URL.
      */
      static get DATABASE_URL() {
-        const port = 1337; // Change this to your server port
-        return `http://localhost:${port}`;
+        // const port = 1337; // Change this to your server port
+        // return `http://localhost:${port}`;
+        return `https://git.heroku.com/mws-restaurant-review-app.git`;
       }
 
     /**
